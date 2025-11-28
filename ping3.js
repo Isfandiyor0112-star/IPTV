d22af68d45ebc582657abd026500ed61331a4de9
Isfandiyor0112-star <imamalievisfandier@gmail.com>
2025-11-28 16:16:12 +0500

    Create ping3.js

---
+++ ping3.js
@@ -0,0 +1,14 @@
+import fetch from "node-fetch";
+
+const URL = "https://st.uzlive.ru/sportuztv/tracks-v1a1/mono.ts.m3u8";
+
+async function ping() {
+  try {
+    const res = await fetch(URL);
+    console.log("Ping:", res.status);
+  } catch (err) {
+    console.error("Ошибка:", err.message);
+  }
+}
+
+setInterval(ping, 5000); // каждые 10 секунд
